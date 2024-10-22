3697e0f91c11829bd719fac17bab63416d4d2c9f
katrine <eaa23kch@students.eaaa.dk>
2024-10-22 11:49:11 +0200

    Downloaded Product app and made alterations according to task 6.7

---
+++ Session 12-startupcode/Controllers/Exercise01Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson02_Startup.Controllers
+{
+    public class Exercise01Controller : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            // create a new product object with instance name glass
+            Product glass = new Product("Wine glass", 160.50);
+            glass.ImageUrl = "grandcru.jpg";
+            ViewBag.Glass = glass;
+
+            //bin
+            Product bin = new Product("Trash bin", 50);
+            bin.ImageUrl = "bin_35l.jpg";
+            ViewBag.Bin = bin;
+
+            //knife
+            Product knife = new Product("Knife", 100);
+            knife.ImageUrl = "st_knife.jpg";
+            ViewBag.Knife = knife;
+			
+            return View();
+        }
+
+    }
+}
---
+++ Session 12-startupcode/Models/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Product
+{
+    private string name; // field
+    public string Name // property
+    {
+        get { return name; }
+    }
+
+    private double price; // field
+    public double Price // property
+    { 
+        // 
+        set {
+            if (value <= 0)
+            { 
+                throw new Exception("Price is not accepted"); 
+            } 
+            else { 
+                price = value; 
+            } 
+        } 
+        get { return price; } 
+    }
+
+
+    private string imageUrl; // field
+    public string ImageUrl // property
+    {
+        get { return imageUrl; }
+        set { imageUrl = value; }
+    }
+
+    private string manufacturer; // field
+    public string Manufacturer { get; set; } //property
+
+    // constructor 1
+    public Product(string name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    // constructor 2
+    public Product(string name, double price, string imageUrl, string manufacturer)
+    {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.manufacturer = manufacturer;
+    }
+}
