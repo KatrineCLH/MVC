85bb6d46d5ae0fc9f6689f178a3b5664f527b42a
katrine <eaa23kch@students.eaaa.dk>
2024-10-22 10:42:37 +0200

    Made a small seconds calculator to practice ViewBag

---
+++ Calculator64/Calculator64/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator64.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        [HttpGet]
+        public ActionResult TimeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TimeCalculator(FormCollection formCollection)
+        {
+            int hours = Convert.ToInt32(formCollection["Hours"]);
+            int minutes = Convert.ToInt32(formCollection["Minutes"]);
+            int seconds = Convert.ToInt32(formCollection["Seconds"]);
+
+            TimeSpan ts = new TimeSpan(0, hours, minutes, seconds);
+            double total = ts.TotalSeconds;
+
+            ViewBag.Hours = hours;
+            ViewBag.Minutes = minutes;
+            ViewBag.Seconds = seconds;
+            ViewBag.Total = total;
+
+            return View("TimeCalculatorResult");
+            //return View();
+        }
+    }
+}
