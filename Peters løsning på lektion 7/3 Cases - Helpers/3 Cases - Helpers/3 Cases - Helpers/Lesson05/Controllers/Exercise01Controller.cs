a5b77c728f9dd5fbadc83fa56082c745f1fe829e
katrine <eaa23kch@students.eaaa.dk>
2024-10-29 10:38:30 +0100

    Teacher's solution for tasks in lesson 7

---
+++ Peters løsning på lektion 7/3 Cases - Helpers/3 Cases - Helpers/3 Cases - Helpers/Lesson05/Controllers/Exercise01Controller.cs
@@ -0,0 +1,62 @@
+﻿using Lesson04.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lesson04.Controllers
+{
+    public class Exercise01Controller : Controller
+    {
+        //
+        // GET: /Exercise01/
+
+        private List<SelectListItem> countryList = new List<SelectListItem>();
+        private int MyId = 0;
+
+        public ActionResult Index(string List)
+        {
+            // if the session variable isn't set you create the list and store it in a session
+            if (Session["countryList"] == null) {
+                countryList.Add(new SelectListItem { Text = "Denmark", Value = "DK", Selected = true });
+                countryList.Add(new SelectListItem { Text = "France", Value = "FR" });
+                countryList.Add(new SelectListItem { Text = "USA", Value = "US" });
+                countryList.Add(new SelectListItem { Text = "China", Value = "CN" });
+                Session["countryList"] = countryList;
+            }
+            else {
+                // if the sessionvariale is set you'll make a reference to it
+                // note that session variable is not strongly typed
+                // that's because all types of variables can be stored in sessions
+                // therefore you must typecast it to the right type when you assign it to a variable
+                countryList = (List<SelectListItem>)Session["countryList"];
+            }
+
+            ViewBag.List = countryList;
+            ViewBag.CountryCode = List;
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formData) {
+
+            countryList = (List<SelectListItem>)Session["countryList"];
+
+            countryList.Add(new SelectListItem { Text = formData["country"], Value = formData["code"], Selected=true});
+            Utilities.SortSelectList(countryList, formData["code"]);
+
+            ViewBag.Country = formData["country"];
+            ViewBag.Code = formData["code"];
+
+            ViewBag.List = countryList;
+            
+
+            return View();
+        }
+
+    }
+}
---
+++ Peters løsning på lektion 7/3 Cases - Helpers/3 Cases - Helpers/3 Cases - Helpers/Lesson05/Controllers/Exercise03Controller.cs
@@ -0,0 +1,73 @@
+﻿using Lesson05.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson05.Controllers
+{
+    public class Exercise03Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            ParkingTicketMachine ptm = new ParkingTicketMachine();
+            ViewBag.Payback = false; 
+            return View(ptm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formData) {
+
+            // create a new instance of ParkingTicketMachine
+            ParkingTicketMachine ptm = new ParkingTicketMachine();
+            
+            // declare a variable which keeps track of the amount inserted
+            int AmountInserted;
+            ViewBag.Payback = false;
+            ViewBag.PaybackAmount = 0;
+            
+            // if the form field representing the amount is not null 
+            if (formData["AmountInserted"] != null) {
+                AmountInserted = Convert.ToInt16(formData["AmountInserted"]); // read the value of the amount inserted
+            }
+            else {
+                AmountInserted = 0; // otherwise set the initial value to 0
+            }
+            
+            if (formData["1"] != null) {
+                // call the insertCoin method with 1+AmountInserted as as parameter
+                ptm.insertCoin(1 + AmountInserted);
+            }
+            else if (formData["2"] != null) {
+                ptm.insertCoin(2 + AmountInserted);
+            }
+            else if (formData["5"] != null) {
+                ptm.insertCoin(5 + AmountInserted);
+            }
+            else if (formData["10"] != null) {
+                ptm.insertCoin(10 + AmountInserted);
+            }
+            else if (formData["20"] != null) {
+                ptm.insertCoin(20 + AmountInserted);
+            }
+
+            if (formData["cancel"] != null) {
+                // reset the model to its initial state
+                ViewBag.Payback = true;
+                ViewBag.PaybackAmount = AmountInserted;
+                ptm = new ParkingTicketMachine();
+            }
+            if (formData["confirm"] != null) {
+                ptm.insertCoin(AmountInserted);
+                // load the receipt view named "confirm" with the model as parameter
+                return View("confirm", ptm);
+            }
+
+            // load the default view with the model as parameter
+            return View(ptm);
+        }
+
+
+    }
+}
