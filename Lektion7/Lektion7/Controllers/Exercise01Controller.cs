f062853fab3a824bb1126af88a65d2a9ccdd78dc
katrine <eaa23kch@students.eaaa.dk>
2024-11-03 21:42:49 +0100

    Setup for exercise 1 in lesson 7. Got stuck on using an action method with a parameter

---
+++ Lektion7/Lektion7/Controllers/Exercise01Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lektion7.Controllers
+{
+    public class Exercise01Controller : Controller
+    {
+        public ActionResult Index(string Country)
+        {
+            List<SelectListItem> countries = new List<SelectListItem>();
+            countries.Add(new SelectListItem { Text = "China", Value = "CN" });
+            countries.Add(new SelectListItem { Text = "Denmark", Value = "DK" });
+            countries.Add(new SelectListItem { Text = "United States of America", Value = "US"});
+
+            ViewBag.Countries = countries;
+            ViewBag.Title = "Countries";
+            ViewBag.CountryCode = Country;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
