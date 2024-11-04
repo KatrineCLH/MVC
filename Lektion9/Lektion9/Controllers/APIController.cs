fd9d096616f41a1a15fb01bfb0fbf88d56210430
katrine <eaa23kch@students.eaaa.dk>
2024-11-04 23:14:30 +0100

    Did task 1 + 2 +3 of lesson 9 to practice endpoints in C#. Tested in Postman as weeeeell

---
+++ Lektion9/Lektion9/Controllers/APIController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.Http;
+using Lektion9.Models;
+
+namespace Lektion9.Controllers
+{
+    public class APIController : ApiController
+    {
+
+        [HttpGet]
+        public List<Person> getPersoner()
+        {
+            List<Person> personer = new List<Person>();
+            personer.Add(new Person(1, "Hans", 20));
+            personer.Add(new Person(2, "Ole", 30));
+            personer.Add(new Person(3, "Gert", 40));
+
+            return personer;
+        }
+
+        [HttpGet]
+        public Person getPersonById(int id)
+        {
+            Person foundPerson = null;
+            
+            List<Person> personer = new List<Person>();
+            personer.Add(new Person(1, "Hans", 20));
+            personer.Add(new Person(2, "Ole", 30));
+            personer.Add(new Person(3, "Gert", 40));
+
+            bool found = false;
+            int i = 0;
+
+            while (!found)
+            {
+                if (personer[i].Id == id) 
+                { 
+                    found = true;
+                    foundPerson = personer[i];
+                }
+                else { i++; }
+            }
+
+            return foundPerson;
+        }
+
+        [HttpPost]
+        public void addPerson(Person p)
+        {
+            List<Person> personer = new List<Person>();
+            personer.Add(new Person(1, "Hans", 20));
+            personer.Add(new Person(2, "Ole", 30));
+            personer.Add(new Person(3, "Gert", 40));
+            personer.Add(p);
+
+            //Console.WriteLine("Der blev tilføjet en person med navn: " + p.Name);
+        }
+    }
+}
---
+++ Lektion9/Lektion9/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lektion9.Models
+{
+    public class Person
+    {
+        private int id;
+        private string name;
+        private int age;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }  
+        
+        public Person(int id, string name, int age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
