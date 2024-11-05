42f1a9603a70876fdf0c8d991ebbe9f81361b6c7
katrine <eaa23kch@students.eaaa.dk>
2024-11-05 08:54:13 +0100

    Setup for exercises in lesson 10

---
+++ Lektion10/Lektion10/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lektion10.Models
+{
+    public class Employee
+    {
+        private int employeeID;
+        private string name;
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public Employee() { }
+    }
+}
